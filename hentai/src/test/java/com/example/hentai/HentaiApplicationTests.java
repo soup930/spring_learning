7172414d26fc3aa8f50b35069e912b9a45367a76
Klock <soup930@naver.com>
2022-04-12 20:20:08 +0900

    first commit

---
+++ hentai/src/test/java/com/example/hentai/HentaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hentai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HentaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
